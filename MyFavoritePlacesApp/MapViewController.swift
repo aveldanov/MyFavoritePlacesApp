ac89ccb831e5658e399174c0bc8dd599e44f8baf
Anton Veldanov <anton.veldanov@gmail.com>
2020-05-01 11:42:45 -0700

    iOS

--- MyFavoritePlacesApp/MapViewController.swift
+++ MyFavoritePlacesApp/MapViewController.swift
@@ -7,24 +7,187 @@
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, CLLocationManagerDelegate {
+  
+  
+  
+  @IBOutlet weak var mapOutlet: MKMapView!
+  
+  var manager = CLLocationManager()
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // long press gesture recognizer
+    
+    let uilpgr = UILongPressGestureRecognizer(target: self, action: #selector(longpress))
+    // 2sec
+    uilpgr.minimumPressDuration = 2
+    
+    mapOutlet.addGestureRecognizer(uilpgr)
+    
+    
+    if activePlace == -1{
+      manager.delegate = self
+      manager.desiredAccuracy = kCLLocationAccuracyBest
+      manager.requestWhenInUseAuthorization()
+      manager.startUpdatingLocation()
+      
+      
+    }else{
+      // place details to display
+      if places.count > activePlace{
+        if let name = places[activePlace]["name"]{
+          if let lat = places[activePlace]["lat"]{
+            if let lon = places[activePlace]["lon"]{
+              
+              if let latitude = Double(lat){
+                if let longitude = Double(lon){
+                  let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+                  let coordinate = CLLocationCoordinate2D(latitude: latitude, longitude: longitude)
+                  
+                  let region = MKCoordinateRegion(center: coordinate, span: span)
+                  
+                  mapOutlet.setRegion(region, animated: true)
+                  
+                  let annotation = MKPointAnnotation()
+                  annotation.coordinate = coordinate
+                  annotation.title = name
+                  mapOutlet.addAnnotation(annotation)
+                }
+                
+                
+              }
+              
+              
+            }
+          }
+        }
+        
+        
+      }
+      
+    }
+  }
+  
+  
+  
+}
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+extension MapViewController{
+  
+  
+  
+  @objc func longpress(gestureRecognizer: UIGestureRecognizer){
+    if gestureRecognizer.state == UIGestureRecognizer.State.began{
+      
+      let touchPoint = gestureRecognizer.location(in: self.mapOutlet)
+      let newCoordinate = mapOutlet.convert(touchPoint, toCoordinateFrom: mapOutlet)
+      
+      //       print(newCoordinate)
+      // Get location name
+      
+      let location = CLLocation(latitude: newCoordinate.latitude, longitude: newCoordinate.longitude)
+      var title = ""
+      
+      CLGeocoder().reverseGeocodeLocation(location) { (placemarks, error) in
+        if error != nil{
+          print(error)
+        }else{
+          if let placemark = placemarks?[0]{
+            //Additional street-level information for the placemark.
+            //             var subThoroughfare = ""
+            //             if placemark.subThoroughfare != nil{
+            //               subThoroughfare = placemark.subThoroughfare!
+            //               
+            //               // append to the address line
+            //               title += subThoroughfare + " "
+            //             }
+            //             
+            //             //The street address associated with the placemark.
+            //             var thoroughfare = ""
+            //             if placemark.thoroughfare != nil{
+            //               thoroughfare = placemark.thoroughfare!
+            //               title += thoroughfare + "\n"
+            //             }
+            
+            //Additional city-level information for the placemark.
+            var subLocality = ""
+            if placemark.subLocality != nil{
+              subLocality = placemark.subLocality!
+              title += subLocality + ", "
+            }
+            
+            
+            
+            
+            //            The state or province associated with the placemark.
+            var administrativeArea = ""
+            if placemark.administrativeArea != nil{
+              administrativeArea = placemark.administrativeArea!
+              title += administrativeArea + " - "
+              
+            }
+            
+            
+            //            The name of the country associated with the placemark.
+            var country = ""
+            if placemark.country != nil{
+              country = placemark.country!
+              title += country
+
+            }
+            
+            print(title)
+          }
+          
+          
+        }
+        print("Title:",title)
+        
+        // no address found
+        if title == ""{
+          print("NOT TITLE FOUND")
+          title = "Added \(NSDate())"
+          print("Title:",title)
+        }
+        
+        print("SUPERTitle:",title)
+        let annotation = MKPointAnnotation()
+        annotation.coordinate = newCoordinate
+        annotation.title = title
+        self.mapOutlet.addAnnotation(annotation)
+        
+        places.append(["name":title, "lat":String(newCoordinate.latitude),"lon":String(newCoordinate.longitude)])
+        defaults.set(places, forKey: "places")
+
+
+        print(places)
+        
+        
+      }
+      
+      
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  }
+  
+  
+  
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    let location = CLLocationCoordinate2D(latitude: locations[0].coordinate.latitude, longitude: locations[0].coordinate.longitude)
+    let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+    let region = MKCoordinateRegion(center: location, span: span)
+    
+    mapOutlet.setRegion(region, animated: true)
+    
+  }
+  
+  
+  
+  
 }
--- MyFavoritePlacesApp/PlacesTableViewController.swift
+++ MyFavoritePlacesApp/PlacesTableViewController.swift
@@ -8,83 +8,145 @@
 
 import UIKit
 
+
+//var places = [String:String]()
+var places = [Dictionary<String,String>()]
+var activePlace = -1
+let defaults = UserDefaults.standard
+
+
 class PlacesTableViewController: UITableViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+  
+  @IBOutlet var tableOutlet: UITableView!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    
+    
+  }
+  
+  
+  override func viewDidAppear(_ animated: Bool) {
+    //    print("PLACES",places)
+    //    print("PLACES COUNT", places.count)
+    //    print("PLACES[0]",places[0])
+    //    print("PLACES[0].COUNT",places[0].count)
+    
+    if let tempPlaces = defaults.object(forKey: "places") as? [Dictionary<String,String>]{
+      places = tempPlaces
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+    if places.count == 1 && places[0].count == 0{
+      places.remove(at: 0)
+      places.append(["name":"Taj Mahal", "lat":"27.1751","lon":"78.0421"])
+      defaults.set(places, forKey: "places")
     }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+    activePlace = -1
+    tableOutlet.reloadData()
+  }
+  
+  
+  
+  
+  // MARK: - Table view data source
+  
+  override func numberOfSections(in tableView: UITableView) -> Int {
+    // #warning Incomplete implementation, return the number of sections
+    return 1
+  }
+  
+  override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    // #warning Incomplete implementation, return the number of rows
+    return places.count
+  }
+  
+  
+  override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    //        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+    let cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
+    
+    if places[indexPath.row]["name"] != nil{
+      cell.textLabel?.text = places[indexPath.row]["name"]
     }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+    
+    
+    return cell
+  }
+  
+  
+  override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+    return true
+  }
+  
+  override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    if editingStyle == UITableViewCell.EditingStyle.delete{
+      places.remove(at: indexPath.row)
+      defaults.set(places, forKey: "places")
+      tableOutlet.reloadData()
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    activePlace = indexPath.row
+    
+    
+    
+    performSegue(withIdentifier: "toMap", sender: nil)
+  }
+  
+  
+  
+  /*
+   // Override to support conditional editing of the table view.
+   override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+   // Return false if you do not want the specified item to be editable.
+   return true
+   }
+   */
+  
+  /*
+   // Override to support editing the table view.
+   override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+   if editingStyle == .delete {
+   // Delete the row from the data source
+   tableView.deleteRows(at: [indexPath], with: .fade)
+   } else if editingStyle == .insert {
+   // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+   }
+   }
+   */
+  
+  /*
+   // Override to support rearranging the table view.
+   override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+   
+   }
+   */
+  
+  /*
+   // Override to support conditional rearranging of the table view.
+   override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+   // Return false if you do not want the item to be re-orderable.
+   return true
+   }
+   */
+  
+  /*
+   // MARK: - Navigation
+   
+   // In a storyboard-based application, you will often want to do a little preparation before navigation
+   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+   // Get the new view controller using segue.destination.
+   // Pass the selected object to the new view controller.
+   }
+   */
+  
 }
